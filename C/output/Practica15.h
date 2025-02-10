0ef4dba7d4f27d4c7dd1ed27c15040d84136aeb4
Jurdi5 <jorge.urdiales@iteso.mx>
2025-02-10 10:39:30 -0600

    Delete C directory

--- C/output/Practica15.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATS_H_
-#define STAT_H_
-
-int max(int a, int b);
-int min(int a, int b, int c);
-float median(int a, int b, int c);
-
-#endif /* STAT_H_*/
--- C/output/Prueba.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main (){
-    printf("Hola,\n");
-    printf("Tarea, Jorge Urdiales, \n");
-    system("PAUSE");
-    return 0;
-}
